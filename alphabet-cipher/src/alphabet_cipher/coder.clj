31e12c753233f41ceb95f767993bc3e11899b4c2
Brent Brimhall <brent.brimhall@gmail.com>
2016-08-07 22:30:44 -0700

    refactored

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,32 +1,24 @@
 (ns alphabet-cipher.coder
   (require [clojure.string :as string]))
 
-; (for [x (range 26)
-;       y (range y (+ 26 y))])
-
 (def looped-alphabet
   (->> (+ 26 97) ;; char code for z
        (range 97) ;; char code for a
        (cycle) ;; forever!!
        (map (comp str char)))) ;; letter
 
-(println (take (* 3 26) looped-alphabet))
-
 (defn letter-idx [x]
-  (.indexOf looped-alphabet x))
-
-(defn chart-row [x]
-  (take 26 (drop (letter-idx x) looped-alphabet)))
+  (.indexOf looped-alphabet (str x)))
 
 (defn encode-value [k m]
-  (nth
-    (chart-row k)
-    (letter-idx m)))
+  (nth looped-alphabet
+       (+ (letter-idx k) (letter-idx m))))
 
 (defn encode [keyword message]
-  (string/join (map (fn [a b] (encode-value (str a) (str b)))
-                    message
-                    (take (count message) (cycle keyword)))))
+  (string/join
+    (map encode-value
+         message
+         (cycle keyword))))
 
 (defn decode [keyword message]
   "decodeme")
