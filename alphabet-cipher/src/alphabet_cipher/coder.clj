3ce25f1f9f64b38f0ca6eb8563f159785140fc52
Brent Brimhall <brent.brimhall@gmail.com>
2016-08-08 00:24:26 -0700

    Decoding works, more refactoring.

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -2,26 +2,39 @@
   (require [clojure.string :as string]))
 
 (def looped-alphabet
-  (->> (+ 26 97) ;; char code for z
-       (range 97) ;; char code for a
-       (cycle) ;; forever!!
-       (map (comp str char)))) ;; letter
+  "(concat (a-z) (a-z))"
+  (let [a 97
+        z (+ 26 97)]
+    (->> (range a z)
+         (map (comp str char)) ;; letter 
+         cycle))) ;; forever!! 
 
-(defn letter-idx [x]
-  (.indexOf looped-alphabet (str x)))
+(defn letter-idx
+  "Gives the index of a letter in a looping alphabet, optionally with an offset."
+  ([x] (.indexOf looped-alphabet (str x)))
+  ([offset x] (.indexOf (drop (letter-idx offset) looped-alphabet) (str x)))) 
 
 (defn encode-value [k m]
   (nth looped-alphabet
        (+ (letter-idx k) (letter-idx m))))
 
+(defn decode-value
+  "find index of m at row k"
+  [k m]
+  (nth looped-alphabet
+       (letter-idx k m)))
+
 (defn encode [keyword message]
   (string/join
     (map encode-value
-         message
-         (cycle keyword))))
+         (cycle keyword)
+         message)))
 
 (defn decode [keyword message]
-  "decodeme")
+  (string/join
+    (map decode-value
+         (cycle keyword)
+         message)))
 
 (defn decipher [cipher message]
   "decypherme")
