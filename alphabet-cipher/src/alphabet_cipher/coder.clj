cfb25031c1bccbebc8bdf62eac89fd24c7169405
Brent Brimhall <brent.brimhall@gmail.com>
2016-08-07 22:24:36 -0700

    refactoring...

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,19 +1,30 @@
 (ns alphabet-cipher.coder
   (require [clojure.string :as string]))
 
-(def substitution-chart
-  (mapv (fn [xs] (mapv #(str (char %)) xs))
-        (mapv
-          (fn [n] (take 26 (drop n (cycle (range 97 (+ 26 97))))))
-          (range 26))))
+; (for [x (range 26)
+;       y (range y (+ 26 y))])
 
-(defn val-at [k m]
-  (let [row (first (filter #(= (first %) k) substitution-chart))
-        col (.indexOf (first substitution-chart) m)]
-    (nth row col)))
+(def looped-alphabet
+  (->> (+ 26 97) ;; char code for z
+       (range 97) ;; char code for a
+       (cycle) ;; forever!!
+       (map (comp str char)))) ;; letter
+
+(println (take (* 3 26) looped-alphabet))
+
+(defn letter-idx [x]
+  (.indexOf looped-alphabet x))
+
+(defn chart-row [x]
+  (take 26 (drop (letter-idx x) looped-alphabet)))
+
+(defn encode-value [k m]
+  (nth
+    (chart-row k)
+    (letter-idx m)))
 
 (defn encode [keyword message]
-  (string/join (map (fn [a b] (val-at (str a) (str b)))
+  (string/join (map (fn [a b] (encode-value (str a) (str b)))
                     message
                     (take (count message) (cycle keyword)))))
 
