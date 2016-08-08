374cf9edc832a4e8160e0d9d7082e3bbebae879e
Brent Brimhall <brent.brimhall@gmail.com>
2016-08-07 22:01:46 -0700

    alphabet cider encode works!

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,7 +1,21 @@
-(ns alphabet-cipher.coder)
+(ns alphabet-cipher.coder
+  (require [clojure.string :as string]))
+
+(def substitution-chart
+  (mapv (fn [xs] (mapv #(str (char %)) xs))
+        (mapv
+          (fn [n] (take 26 (drop n (cycle (range 97 (+ 26 97))))))
+          (range 26))))
+
+(defn val-at [k m]
+  (let [row (first (filter #(= (first %) k) substitution-chart))
+        col (.indexOf (first substitution-chart) m)]
+    (nth row col)))
 
 (defn encode [keyword message]
-  "encodeme")
+  (string/join (map (fn [a b] (val-at (str a) (str b)))
+                    message
+                    (take (count message) (cycle keyword)))))
 
 (defn decode [keyword message]
   "decodeme")
