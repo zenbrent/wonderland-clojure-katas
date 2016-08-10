0cd2cd02360e02565e3c4cf62b14b1827afed240
Brent Brimhall <brent.brimhall@gmail.com>
2016-08-09 21:55:15 -0700

    Decipher works...

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -9,10 +9,15 @@
          (map (comp str char)) ;; letter 
          cycle))) ;; forever!! 
 
+(defn letter-idx-of
+  "Gives the index of a letter in a looping alphabet, optionally with an offset."
+  ([str-list x] (.indexOf str-list (str x)))
+  ([str-list offset x] (.indexOf (drop (letter-idx-of str-list offset) str-list) (str x)))) 
+
 (defn letter-idx
   "Gives the index of a letter in a looping alphabet, optionally with an offset."
-  ([x] (.indexOf looped-alphabet (str x)))
-  ([offset x] (.indexOf (drop (letter-idx offset) looped-alphabet) (str x)))) 
+  ([x] (letter-idx-of looped-alphabet x))
+  ([offset x] (letter-idx-of looped-alphabet offset x))) 
 
 (defn encode-value [k m]
   (nth looped-alphabet
@@ -36,6 +41,38 @@
          (cycle keyword)
          message)))
 
-(defn decipher [cipher message]
-  "decypherme")
+(defn match-all-and-start-of-last [items]
+  (if (apply = (map count items))
+    (apply = items)
+    (and (apply = (butlast items))
+         (string/starts-with? (first items) (subvec items (count items))))))
+
+;; (a . . .) (a . . .) (a .)
+;; if you slice them at intervals every (last repeats), and they all match
+;; that's it
+;; if not, slice them at every (second-to-last repeats), and they all match
+;; that's it
+;; if not, slice them at every (third-to-last repeats), and they all match
+;; that's it
+(defn decipher
+  "if the message is shorter than the key, the full key can't be recovered."
+  [cipher message]
+  (let [looped-key (vec (map decode-value
+                             message
+                             cipher))
+        first-char-repeats (reduce-kv
+                             (fn [acc idx values]
+                               (if (= (first looped-key) values)
+                                 (conj acc idx)
+                                 acc))
+                             []
+                             looped-key)
+        string-repeats (mapcat
+                         (fn [n] (let [xs (partition n looped-key)]
+                                   (if (< n (/ (count looped-key) 2))
+                                     [xs]
+                                     [])))
+                         (filter #(not= 0 %) first-char-repeats))
+        matches (map first (filter match-all-and-start-of-last string-repeats))]
+    (string/join (first matches))))
 
